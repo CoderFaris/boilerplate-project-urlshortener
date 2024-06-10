22d518c0779a11929ef28d63a755412adc15faa4
WyngeNotFoundWasTaken <farki.kadric@gmail.com>
2024-06-10 13:57:04 +0200

    changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
+const bodyParser = require('body-parser')
 const express = require('express');
 const cors = require('cors');
+const URL = require("url").URL;
 const app = express();
 
 // Basic Configuration
@@ -10,6 +12,8 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +22,63 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urlDatabase = {};  // short_url -> original_url
+let reverseDatabase = {};  // original_url -> short_url
+let idCounter = 1;
+
+
+const isValidUrl = (s) => {
+
+  try {
+
+    new URL(s);
+    return true;
+  } catch (err) {
+
+    return false
+
+  }
+
+}
+
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+
+  
+  if (!isValidUrl(originalUrl)) {
+
+    res.json({"error" : "invalid url"})
+
+  }
+
+
+  
+  if (reverseDatabase[originalUrl]) {
+    const shortUrl = reverseDatabase[originalUrl];
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  } else {
+    const shortUrl = idCounter++;
+    
+    
+    urlDatabase[shortUrl] = originalUrl;
+    reverseDatabase[originalUrl] = shortUrl;
+    
+    res.json({ original_url: originalUrl, short_url: shortUrl });
+  }
+});
+
+app.get('/api/shorturl/:short_url', function(req, res) {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+  
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({"error" : "no short url found"})
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
